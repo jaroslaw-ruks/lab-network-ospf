cc62e4c836ed02e6247226158b35a8b37d6659ff
Jaroslaw Ruks <jaroslaw.ruks@currency-one.com>
2019-07-01 17:11:42 +0200

    some fixes.

--- README.md
+++ README.md
@@ -4,9 +4,13 @@ simple lab for OSPF traning.
 env
 c=0/1 command impact for client (default 0)
 r=0/1 command impact for routers (defautl 1)
-
-c=1 vagrant up 
+s=0/1 
+c=1 vagrant up # spawn client
+r=1 vagrant up # spawn router 
+s=1 vagrant up # install and confgure bird
+s=2 vagrant up # create static route between routeres
 Spawn client and routers.
  
 For destroy client and routers. (r=1 optional)
 c=1 vagrant destroy
+
--- scripts/routers_part1.sh
+++ scripts/routers_part1.sh
@@ -7,12 +7,14 @@ sysctl -p /etc/sysctl.conf
 apt-get update
 apt-get install vim tcpdump -y
 echo $s
-if [  "$s" -eq 2 ] 
+if [  "$s" -eq 1 ] 
 then
-	/bin/sh /vagrant/scripts/$(hostname).sh > /tmp/log 2>&1
-else 
 	apt-get install bird -y
 	cp /vagrant/scripts/bird.conf /etc/bird/bird.conf
 	sed -i -e "s/A.B.C.D/`hostname -I | awk '{print $NF}'`/g"  /etc/bird/bird.conf 
+	systemctl restart bird.service
+elif [ "$s" -eq 2 ]
+	then
+	/bin/sh /vagrant/scripts/$(hostname).sh > /tmp/log 2>&1
 fi
 
